fc9c9e107e970ed423e33aceff2c7ffc8e2ed2a5
Luke Plant <L.Plant.98@cantab.net>
2013-05-08 23:56:15 +0100

    Fixed Django 1.6 compatibility with urls

--- campaign/urls.py
+++ campaign/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^view/(?P<object_id>[\d]+)/$', 'campaign.views.view_online', {}, name="campaign_view_online"),
